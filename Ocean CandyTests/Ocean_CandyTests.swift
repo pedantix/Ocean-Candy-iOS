b7e84a69bebcbed3e0987a5fb459d6535366edab
Shaun  Hubbard <shaun.hubbard@ascentis.com>
2015-02-19 20:24:14 -0500

    Initial Commit

---
+++ Ocean CandyTests/Ocean_CandyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ocean_CandyTests.swift
+//  Ocean CandyTests
+//
+//  Created by Shaun  Hubbard on 2/19/15.
+//  Copyright (c) 2015 ATEORAMS. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Ocean_CandyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
