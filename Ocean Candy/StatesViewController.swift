4b16334d28f8f6a891ccefa66a435b0ba1adbbb5
Shaun  Hubbard <shaun.hubbard@ascentis.com>
2015-02-19 21:14:10 -0500

    Initiali Commit + Stattion/Stations/States(View Controllers) being set up

---
+++ Ocean Candy/StatesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StatesViewController.swift
+//  Ocean Candy
+//
+//  Created by Shaun  Hubbard on 2/19/15.
+//  Copyright (c) 2015 ATEORAMS. All rights reserved.
+//
+
+import UIKit
+
+class StatesViewController: UIViewController {
+
+}
